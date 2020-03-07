25c61c4ccefa27a25af985cba79f52127c29d1b9
Justin Taylor Weiss <justintaylorweiss@gmail.com>
2020-03-07 01:13:37 -0500

    Setup General Layout and did sample for generate button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import featuredQuoteImg from './assets/featuredQuote.png';
+import redNinja from './assets/redNinja.png';
+import upsidedownNinja from './assets/upsidedownNinja.png';
+import inspirationalNinja from './assets/redText.png';
 import './App.css';
 
 /*
@@ -26,23 +29,34 @@ const handleResult = (r: Result) => {
 
 //https://mercuryannualreport.co.nz/
 
+type quoteColor = "Blue"|"Red"|"Green"|"Black"|"White";
+type colorCyleArray = Array<quoteColor>;
+
+const colorCyle:colorCyleArray = ["Blue","Red","Green","Black"];
+
 function App() {
   const [quoteGenerateNumber, setGenerateButton] = React.useState<number>(0);
+  const generateQuoteStyle = {backgroundColor: colorCyle[quoteGenerateNumber%colorCyle.length]};
   return (
     <div className="App">
+      <div className="navBar">
+        <img src={inspirationalNinja} alt="Inspirational Ninja" className="inspirationalNinjaTextImg"/>
+        <p className="menuButton">car</p>
+      </div>
       <div className ="Container">
-        <div className="Ninja"/>
-        <p className="HeaderText">Our Team Of Underpaid Ninjas Are Ready<br/>To Conjure Inspirational Quotes That<br/>Will Spark Your Imaginiation, Ease Your<br/>Mind, And Warm Your Heart.</p>
+        <img src={redNinja} alt="Red Ninja" className="TopNinja"/>
+        <p className="HeaderText">Our team of underpaid ninjas are ready<br/>to conjure inspirational quotes that<br/>will spark your imaginiation, ease your<br/>mind, and warm your heart.</p>
         <button className="GenerateButton" type="button" onClick={()=>setGenerateButton(quoteGenerateNumber+1)}>Get Inspired</button>
-        {(quoteGenerateNumber!==0) ? <div className="generatedQuote"></div> : null}
+        {(quoteGenerateNumber!==0) ? <div className="GeneratedQuote" style={generateQuoteStyle}></div> : null}
         <div className="Line"/>
-        <h1 className="FeaturedQuote">Featured Quote</h1>
-        <div className="QuoteImage"/>
+        <h1 className="FeaturedQuoteHeader">Featured Quote</h1>
+        <img src={featuredQuoteImg} alt="Featured Quote" className="FeaturedQuoteImage"/>
         <div className="SocialMediaSection">
+          <img src={upsidedownNinja} alt="Upsidedown Ninja" className="UpsidedownNinja"/>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
